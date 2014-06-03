35b62f75e1d163e1381591ff885272daa9c2bca7
bijugs <vagrant@bcpc-bootstrap>
2014-06-03 20:29:25 +0000

    Minor change to automated_install.sh to support Mac installation

--- tests/automated_install.sh
+++ tests/automated_install.sh
@@ -23,14 +23,14 @@ export CLUSTER_VM_CPUs=4
 
 printf "#### Setup configuration files\n"
 # setup vagrant
-sed -i"" 's/vb.gui = true/vb.gui = false/' Vagrantfile
+sed -i "" 's/vb.gui = true/vb.gui = false/' Vagrantfile
 
 # setup proxy_setup.sh
 [[ -n "$PROXY" ]] && sed -i"" "s/#export PROXY=.*\"/export PROXY=\"$PROXY\"/" proxy_setup.sh
 
 # setup environment file
-sed -i"" "s/\"dns_servers\" : \[ \"8.8.8.8\", \"8.8.4.4\" \]/\"dns_servers\" : \[ $DNS_SERVERS \]/" environments/${ENVIRONMENT}.json
-[[ -n "$PROXY" ]] && sed -i"" "s#\(\"bootstrap\": {\)#\1\n\"proxy\" : \"http://$PROXY\",\n#" environments/${ENVIRONMENT}.json
+sed -i "" "s/\"dns_servers\" : \[ \"8.8.8.8\", \"8.8.4.4\" \]/\"dns_servers\" : \[ $DNS_SERVERS \]/" environments/${ENVIRONMENT}.json
+[[ -n "$PROXY" ]] && sed -i "" "s#\(\"bootstrap\": {\)#\1\n\"proxy\" : \"http://$PROXY\",\n#" environments/${ENVIRONMENT}.json
 
 printf "#### Setup VB's and Bootstrap\n"
 source ./vbox_create.sh
